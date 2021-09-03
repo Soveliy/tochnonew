0132e1a21bca658e6ba101516b6f25728d19a93a
Sovel <nikita.elagin2012@yandex.ru>
2021-09-03 16:35:19 +0300

    urapde

--- js/main.js
+++ js/main.js
@@ -2,24 +2,27 @@
 
 $(function () {
   jQuery(document).ready(function ($) {
-    $("form").submit(function (e) {
+    
+    $(".btn-outline-black.chat").click(function (e) {
       e.preventDefault();
-      var form_data = $(this).serialize();
-      $.ajax({
-        type: "POST",
-        url: "send.php",
-        data: form_data,
-        success: function success() {
-          $(".modal").modal('hide');
-
-          if ($('.nav a.active').hasClass('senk')) {
-            $("#senk").modal('show');
-          } else {
-            $("#senk1").modal('show');
-          }
-        }
-      });
+      // window.usedeskMessenger.open('chat');
+      $('#quest').modal('show');
     });
+
+    $(".btns__link.chat").click(function (e) {
+      e.preventDefault();
+      window.usedeskMessenger.openChat();
+       window.usedeskMessenger.open('chat');
+      $(".fiULNP").trigger("click");
+    });
+    $("form .nav li a").click(function(){
+      let pm = $(this).text();
+      console.log(pm)
+      $("#for-radios").val(pm)
+      console.log($("#for-radios").val())
+    })
+
+    
     $('.slider-contact').slick({
       infinite: false,
       arrows: true,
@@ -72,8 +75,54 @@ $(function () {
     $("[name='colvo']").on('keyup', function () {
       $(this).val($(this).val().replace(/[^0-9]/g, ''));
     }); //$(".scroll").mCustomScrollbar();
-
+   
     $("[name='phone']").mask("+7 (999) 999-99-99");
+
+    $("[name='phone']").on('keyup', function () {
+      let phone = $(this).val()
+      $("#for-phone").val(phone)
+    }); 
+  
+    $(".form_options").click(function(){
+    
+
+
+      var checkboxes = [];
+      $('.form_options input:checked').each(function(){
+        //добавляем значение каждого флажка в этот массив
+
+      });
+      /*объединяем массив в строку с разделителем-запятой. Но лучше подобные вещи хранить в массиве. Для наглядности - вывод в консоль.*/
+  
+      $("#for-profs").val((checkboxes.join(',')))
+    })
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = dd + '/' + mm + '/' + yyyy;
+   
+   
+    $("#for-date").val(today)
+
+    $(".nav li:first-child").click(function(){
+      $("#for-date2").val('')
+      $("#for-date").val(today)
+    })
+    $(".nav li:nth-child(2)").click(function(){
+      $("#for-date2").val(today)
+      $("#for-date").val('')
+    })
+    $("[name='name']").on('keyup', function () {
+      let name = $(this).val()
+      $("#for-name").val(name)
+    }); 
+    $("[name='colvo']").on('keyup', function () {
+      let colvo = $(this).val()
+      $("#for-colvo").val(colvo)
+    }); 
     $('a[href^="#"].yak').click(function () {
       var elementClick = $(this).attr("href");
       elementClick = elementClick.substr(1);
@@ -138,16 +187,18 @@ $(function () {
     });
     $("#prof li").click(function () {
       $('.modal').modal('hide');
+      $("#for-prof").val($(this).text());
       $("[name='prof']").val($(this).text());
+      $("[name='prof']").addClass("active");
+
     });
     $("#graj li").click(function () {
       $('.modal').modal('hide');
       $("[name='graj']").val($(this).text());
+      $("[name='graj']").addClass("active");
+      $("#for-graj").val($(this).text());
     });
-    $(".chat").click(function (e) {
-      e.preventDefault();
-      window.usedeskMessenger.open('chat');
-    });
+   
     $('.mob-menu ').click(function () {
       $(this).toggleClass('active');
       $('.menu').slideToggle(300);
@@ -211,3 +262,186 @@ $(".modal-content__close").click(function (){
 });
 
 
+$("#callback form").validate({
+  rules: {
+    name: "required",
+    phone: "required",
+    
+  },
+  messages: {
+    name: "Это поле обязательно к заполнению",
+    phone: "Это поле обязательно к заполнению",
+    
+  },
+
+  submitHandler: function() {
+   
+    var form_data = $("#callback form").serialize();
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: form_data,
+        success: function success() {
+          $(".modal").modal('hide');
+    
+          if ($('.nav a.active').hasClass('senk')) {
+            $("#senk").modal('show');
+          } else {
+            $("#senk1").modal('show');
+          }
+        }
+      });
+
+    return false;
+}
+
+});
+$(".oformlenie form").validate({
+  rules: {
+    name: "required",
+    phone: "required",
+    
+  },
+  messages: {
+    name: "Это поле обязательно к заполнению",
+    phone: "Это поле обязательно к заполнению",
+    
+  },
+
+  submitHandler: function() {
+   
+    var form_data = $(".oformlenie form").serialize();
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: form_data,
+        success: function success() {
+          $(".modal").modal('hide');
+    
+          if ($('.nav a.active').hasClass('senk')) {
+            $("#senk").modal('show');
+          } else {
+            $("#senk1").modal('show');
+          }
+        }
+      });
+
+    return false;
+}
+
+});
+
+
+$(function() {
+  $(".oformlenie form").submit(function (event) {
+    event.preventDefault();
+ 
+    // Ссылка, которую получили на этапе публикации приложения
+    let appLink = "https://script.google.com/macros/s/AKfycbw9FjaZZDLgTVcSWKcX62JKRYaTxntpTavHYjgZO6jPtl1Rpk9Il4DOQI2IpLpsidUG2Q/exec";
+ 
+    // Сообщение при успешной отправке данных
+    let successRespond = 'Сообщение успешно отправлено. Посмотрите результат <a target="_blank" href="https://docs.google.com/spreadsheets/d/1XcTivCQL4EZJf1x2kB4UwGj5bAi0sfpx1PwtaKgOWHo/edit?usp=sharing">тут</a>';
+ 
+    // Сообщение при ошибке в отправке данных
+    let errorRespond = 'Не удалось отправить сообщение. Cвяжитесь с администратором сайта по адресу <a href="mailto:smart-landing@ya.ru">smart-landing@ya.ru</a>';
+ 
+    // Id текущей формы
+    let form = $('#' + $(this).attr('id'))[0];
+ 
+    // h2 с ответом формы
+    let formRespond = $(this).find('.g-form__title_respond');
+ 
+    // h2 с заголовком формы
+    let formTitle = $(this).find('.g-form__title_main');
+ 
+    // Блок прелоадера
+    let preloader = $(this).find('.g-form__preloader');
+ 
+    // Кнопка отправки формы
+    let submitButton = $(this).find('.g-form__button');
+ 
+ 
+    // FormData
+    let fd = new FormData(form);
+ 
+ 
+    $.ajax({
+ 
+      url: appLink,
+      type: "POST",
+      data: fd,
+      processData: false,
+      contentType: false,
+      beforeSend: function(){
+ 
+      
+      // Показываем прелоадер
+      preloader.css('opacity', '1');
+ 
+      // Делаем неактивной кнопку отправки
+      submitButton.prop('disabled', true);
+ 
+      // валидация других полей.
+ 
+    },
+ 
+  }).done(function(res, textStatus, jqXHR) {
+ 
+    if(jqXHR.readyState === 4 && jqXHR.status === 200) {
+ 
+    // Прячем заголовок формы
+    formTitle.css({
+      'display': 'none'
+    });
+ 
+    // Прячем прелоадер
+    preloader.css('opacity', '0');
+ 
+    // Выводим ответ формы.
+    formRespond.html(successRespond).css('color', '#37b599');
+     
+    // Возвращаем активность кнопке отправки
+    submitButton.prop('disabled', false);
+ 
+      // Очищаем поля формы
+      form.reset();
+ 
+    } else {
+      formTitle.css({
+        'display': 'none'
+      });
+      formRespond.html(errorRespond).css('color', '#c64b4b');
+      preloader.css('opacity', '0');
+      setTimeout( () => {
+        formRespond.css({
+          'display': 'none'
+        });
+        formTitle.css({
+          'display': 'block'
+        });
+ 
+        submitButton.prop('disabled', false);
+      }, 5000);
+ 
+      console.log('Гугл не ответил статусом 200');
+    }
+  }).fail(function(res, textStatus, jqXHR) {
+    formTitle.css({
+      'display': 'none'
+    });
+    preloader.css('opacity', '0');
+    formRespond.html('Не удалось отправить сообщение. Cвяжитесь с администратором сайта другим способом').css('color', '#c64b4b');
+    setTimeout( () => {
+      formRespond.css({
+        'display': 'none'
+      });
+      formTitle.css({
+        'display': 'block'
+      });
+      submitButton.prop('disabled', false); 
+    }, 5000);
+ 
+    console.log('Не удалось выполнить запрос по указанному в скрипте пути');
+  }); 
+});
+}(jQuery));
